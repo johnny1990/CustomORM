821cc4bd50639c25d4a3887830a841f5fcc739fb
Man Ioan <man_ioan@live.com>
2020-09-16 20:33:47 +0300

    Implement functionality for ORM

---
+++ CustomORM/DbConnection/SqlDb.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace CustomORM.DbConnection
+{
+    public static class SqlDb
+    {
+        public static string MainConnectionString
+        {
+            get
+            {
+                return conns != null ? conns : System.Configuration.ConfigurationManager.ConnectionStrings["MainConnection"].ConnectionString;
+            }
+            set
+            {
+                conns = value;
+            }
+        }
+        static string conns = null;
+        public static IDataReader Get(List<SqlParameter> paramsList, string My_SP)
+        {
+
+            SqlConnection con = new SqlConnection(MainConnectionString);
+
+            SqlCommand command = new SqlCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.Connection = con;
+            command.CommandText = My_SP + "_get";
+            command.CommandTimeout = 0;
+            DataTable dt = new DataTable();
+            foreach (var param in paramsList)
+            {
+                command.Parameters.Add(param);
+            }
+            try
+            {
+                con.Open();
+                using (TransactionScope ts = new TransactionScope())
+                {
+                    SqlDataReader reader = command.ExecuteReader();
+                    dt.Load(reader);
+                    ts.Complete();
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.Message.ToString();
+            }
+            finally
+            {
+                con.Close();
+            }
+            return dt.CreateDataReader();
+        }
+
+
+        public static long? Insert(List<SqlParameter> paramsList, string My_SP)
+        {
+            long? ID = null;
+            SqlConnection con = new SqlConnection(MainConnectionString);
+            SqlCommand command = new SqlCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.Connection = con;
+            command.CommandText = My_SP + "_create";
+            command.CommandTimeout = 0;
+            DataTable dt = new DataTable();
+            foreach (var param in paramsList)
+            {
+                command.Parameters.Add(param);
+            }
+            try
+            {
+                con.Open();
+                using (TransactionScope ts = new TransactionScope())
+                {
+                    ID = Convert.ToInt64(command.ExecuteScalar());
+                    ts.Complete();
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.Message.ToString();
+            }
+            finally
+            {
+                con.Close();
+            }
+            return ID;
+
+        }
+
+        public static void Delete(List<SqlParameter> paramsList, string My_SP)
+        {
+            SqlConnection con = new SqlConnection(MainConnectionString);
+
+            SqlCommand command = new SqlCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.Connection = con;
+            command.CommandText = My_SP + "_delete";
+            command.CommandTimeout = 0;
+            DataTable dt = new DataTable();
+            foreach (var param in paramsList)
+            {
+                command.Parameters.Add(param);
+            }
+            try
+            {
+                con.Open();
+                using (TransactionScope ts = new TransactionScope())
+                {
+                    command.ExecuteNonQuery();
+                    ts.Complete();
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.Message.ToString();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public static void Update(List<SqlParameter> paramsList, string My_SP)
+        {
+            SqlConnection con = new SqlConnection(MainConnectionString);
+
+            SqlCommand command = new SqlCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.Connection = con;
+            command.CommandText = My_SP + "_update";
+            command.CommandTimeout = 0;
+            DataTable dt = new DataTable();
+            foreach (var param in paramsList)
+            {
+                command.Parameters.Add(param);
+            }
+            try
+            {
+                con.Open();
+                using (TransactionScope ts = new TransactionScope())
+                {
+                    command.ExecuteNonQuery();
+                    ts.Complete();
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.Message.ToString();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
--- CustomORM/MainWindow.xaml.cs
+++ CustomORM/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using CustomORM.DbConnection;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +24,91 @@ namespace CustomORM
     /// </summary>
     public partial class MainWindow : Window
     {
+        SqlConnectionStringBuilder connbuilder = new SqlConnectionStringBuilder();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            txtResult.Clear();
+            txtResult2.Clear();
+            txtResult3.Clear();
+            txtResInsert.Clear();
+
+            try
+            {
+                SqlDb.MainConnectionString = connbuilder.ConnectionString;
+                IDataReader reader = SqlDb.Get(new List<SqlParameter>(), txtSp.Text);
+
+                txtResult3.Text = "List<SqlParameter> collection = new List<SqlParameter>();" + Environment.NewLine;
+
+
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    string field = reader.GetName(i);
+                    string type = "string";
+                    string conv = ".ToString();";
+                    if (field.Contains("ID"))
+                    {
+                        type = "int?";
+                        conv = ".ToInt();";
+                    }
+                    else if (field.Contains("Date"))
+                    {
+                        type = "DateTime?";
+                        conv = ".ToDate();";
+                    }
+                    txtResult.Text += "public " + type + " " + field + " {get;set;}\r\n";
+
+                    txtResult2.Text += "record." + field + "=reader[\"" + field + "\"]" + conv + Environment.NewLine;
+                    if (!field.Contains("ID"))
+                        txtResult3.Text += "collection.Add(new SqlParameter(\"@" + field + "\", obj." + field + "));" + Environment.NewLine;
+                }
+                txtResult2.Text += "result.Add(record);";
+                txtResult3.Text += "SqlParameter par = new SqlParameter(\"@ID\", SqlDbType.BigInt, sizeof(long));" + Environment.NewLine;
+                txtResult3.Text += "par.Direction = System.Data.ParameterDirection.Output; " + Environment.NewLine;
+                txtResult3.Text += "collection.Add(par);" + Environment.NewLine;
+                txtResult3.Text += "obj.ID = SQLDB.ExecuteInsert(collection, " + txtSp.Text + ").ToLong().Value;" + Environment.NewLine;
+                txtResult3.Text += "return obj; ";
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message); 
+            }
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            connbuilder.DataSource = tb1.Text;
+            connbuilder.InitialCatalog = tb4.Text;
+            connbuilder.UserID = tb3.Text;
+            connbuilder.Password = tb2.Text;
+
+            try
+            {
+                SqlConnection conn = new SqlConnection(connbuilder.ConnectionString);
+                conn.Open();
+                MessageBox.Show("OK");
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] bytes = File.ReadAllBytes(@"D:\Test\Test.xls");
+            string s = "new List<byte>() {";
+            foreach (byte b in bytes)
+                s += b.ToString() + ",";
+            s = s.Substring(0, s.Length - 1);
+            s += "};";
+            tb5.Text = s;
         }
     }
 }
